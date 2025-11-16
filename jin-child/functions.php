74ce73f9ea4f179b0442b9530b1bec6de9933daa
hiroto <tohhiro@gmail.com>
2025-11-16 19:45:48 +0900

    first commit

---
+++ jin-child/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' ); 
+function theme_enqueue_styles() { 
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+} 
+
+ function priority_change_delete(){
+     remove_filter( 'the_content', 'do_shortcode', 1 );
+     add_filter('the_content', 'do_shortcode', 11);
+ }
+ add_action('init', 'priority_change_delete',9999);
