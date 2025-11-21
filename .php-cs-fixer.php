5bf03b808e8e22d170c1b78bfbdb6c1f07dc2d3d
hiroto <tohhiro@gmail.com>
2025-11-21 22:49:04 +0900

    composerをインストールしlinterとformatterを追加 - PHP_CodeSniffer（phpcs, phpcbf） - PHP CS Fixer - WordPress Coding Standards（WPCS）

---
+++ .php-cs-fixer.php
@@ -0,0 +1,27 @@
+<?php
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+return (new Config())
+    ->setRules([
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'binary_operator_spaces' => true,
+        'blank_line_after_opening_tag' => true,
+        'blank_line_before_statement' => [
+            'statements' => ['return'],
+        ],
+        'cast_spaces' => true,
+        'concat_space' => ['spacing' => 'one'],
+        'function_declaration' => [
+            'closure_function_spacing' => 'one',
+        ],
+        'new_with_braces' => true,
+        'no_unused_imports' => true,
+        'single_quote' => true,
+        'trailing_comma_in_multiline' => true,
+    ])
+    ->setFinder(
+        Finder::create()->exclude('vendor')->in([__DIR__])
+    );
--- jin-child/functions.php
+++ jin-child/functions.php
@@ -1,12 +1,24 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' ); 
-function theme_enqueue_styles() { 
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-} 
+/**
+ * Jin Child Theme Functions
+ */
 
- function priority_change_delete(){
-     remove_filter( 'the_content', 'do_shortcode', 1 );
-     add_filter('the_content', 'do_shortcode', 11);
- }
- add_action('init', 'priority_change_delete',9999);
+/**
+ * Enqueue parent and child theme styles
+ */
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+function theme_enqueue_styles()
+{
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+}
+
+/**
+ * Change priority for shortcode processing
+ */
+function priority_change_delete()
+{
+    remove_filter('the_content', 'do_shortcode', 1);
+    add_filter('the_content', 'do_shortcode', 11);
+}
+add_action('init', 'priority_change_delete', 9999);
